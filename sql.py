ac38127c6a1c906889f67f0c1b9b9c4f5a981766
Rudolphcljc <rlagunaj@emeal.nttdata.com>
2024-09-12 13:48:20 -0600

    Se agregó documentación

--- sql.py
+++ sql.py
@@ -1,44 +1,65 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Text, LargeBinary, insert
 from datetime import datetime
 
-# Create engine db
+# Create the database engine
 engine = create_engine('db_url')
 
-# Connect engine
+# Connect to the engine
 conn = engine.connect()
 
 # Set metadata
 metadata = MetaData()
 
-# Table
+# Define the 'inputs' table
 inputs = Table(
     'inputs', metadata,
     Column('Id', Integer, primary_key=True),
-    Column('Timestamp', String(255), nullable=False),
-    Column('User_input', String(255), nullable=False),
-    Column('Response', String(255)),
+    Column('Timestamp', String(), nullable=False),
+    Column('User_input', String(), nullable=False),
+    Column('Response', String()),
     Column('diagram_code', Text, default=''),
     Column('diagram_image', LargeBinary, default=None)
 )
 
-# Crear todas las tablas en la base de datos
+# Create all tables in the database
 metadata.create_all(engine)
 
-# Insertar datos
+# Insert data query
 query = insert(inputs)
 
 def get_values(user_input, lang_answer, image):
+    """
+    Prepare the values to be inserted into the 'inputs' table.
+
+    Args:
+        user_input (str): The user's input.
+        lang_answer (str): The language answer or code.
+        image (bytes): The image data in binary format.
+
+    Returns:
+        list: A list of dictionaries containing the values to be inserted.
+    """
     values_list = [
-        {'Timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 
-         'User_input': user_input, 'Response': False, 
-         'diagram_code': lang_answer,
-         'diagram_image': image}]
+        {
+            'Timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 
+            'User_input': user_input, 
+            'Response': False, 
+            'diagram_code': lang_answer,
+            'diagram_image': image
+        }
+    ]
     
     return values_list
-    
-values_list = get_values()
+
+# Example usage of get_values function
+user_input = "Example input"
+lang_answer = "Example code"
+image = b'Example binary data'
+
+values_list = get_values(user_input, lang_answer, image)
+
+# Execute the insert query
 result = conn.execute(query, values_list)
 
-# Cerrar la conexión
+# Close the connection
 conn.close()
- 
