61c2f42581326941635a7fe9b40b15d61b973710
Rudolphcljc <rlagunaj@emeal.nttdata.com>
2024-09-17 09:50:38 -0600

    added a text box for specifications.

--- front_src/app.py
+++ front_src/app.py
@@ -17,10 +17,17 @@ with col1:
         
         st.write("You selected:", option)
         
+        text_input = st.text_input(
+        "Enter the required specifications",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        placeholder=st.session_state.placeholder,
+    )
+        
         buttom_check = st.form_submit_button(label="Submit")
 
 with col2:
-    if buttom_check and option == 'AWS':
+    if buttom_check and option == 'AWS' and text_input:
         
         url= "https://github.com/Ikelly99/Diagram_creation_model/blob/main/front_src/paper_plane.jpg"
         
@@ -38,7 +45,7 @@ with col2:
             Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
             Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet""")
         
-    elif buttom_check and option == 'GCP':
+    elif buttom_check and option == 'GCP' and text_input:
         url = "https://github.com/Ikelly99/Diagram_creation_model/blob/main/front_src/paper_planes.jpg"
         
         response = requests.get(url)
@@ -55,7 +62,7 @@ with col2:
             Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
             Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet""")
         
-    elif buttom_check and option == 'Azure':
+    elif buttom_check and option == 'Azure' and text_input:
         url = "https://github.com/Ikelly99/Diagram_creation_model/blob/main/front_src/a_lot_of_paper_planes.jpg"
         
         response = requests.get(url)
@@ -71,6 +78,9 @@ with col2:
             Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
             Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
             Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet""")
+        
+    else:
+        st.write("You have not entered the specifications")
 
 
     
