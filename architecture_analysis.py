4192f56fa2f9ac6a15c23ca70b15e8d1e369b12b
Rudolphcljc <rlagunaj@emeal.nttdata.com>
2024-09-18 16:37:16 -0600

    fix some errors.

--- architecture_analysis.py
+++ architecture_analysis.py
@@ -25,10 +25,11 @@ st.set_page_config(
     initial_sidebar_state="collapsed")
 
 col1, col2 = st.columns([1,1])
+
 with col1:
     with st.form("Space_1"):
         image_input = st.file_uploader("Upload An Image", type=['png', 'jpeg', 'jpg'], accept_multiple_files=False)
-        text_input = st.text_area(label="Enter the specifications", height=60)
+        text_input = st.text_area(label="Enter a short description of the diagram", height=60)
         buttom_check = st.form_submit_button(label="Submit")
         if image_input is not None:
             file_details = {"FileName": image_input.name, "FileType": image_input.type}
@@ -40,25 +41,6 @@ with col1:
             st.success("Saved File")
 
 
-with (((col2))):
+with col2:
     if buttom_check and text_input:
-        text_input = text_input
-        # perceptron layer
-        cla = classiffier([text_input])
-        if analyze_prompt(text_input) == "Allowed" and cla == 1:
-            topic, language, is_greeting, allowed = LLMConnect(text_input).connect_client_test()
-            if allowed == "allowed":
-                analyzed_image = LLM_DiagramAnalyzer(r"uploaded_images\saved_image.jpg", text_input)
-                components, service_connections, explanation, is_viable, Advantages_disadvantages = analyzed_image.diagram_analysis()
-                text_out = (f"Service connections: {service_connections}, \n"
-                            f"Explanation: {explanation}, \n"
-                            f"Viability: {is_viable},"
-                            f"Advantages and disadvantages: {Advantages_disadvantages}")
-            else:
-                message = f" Your message was flagged as malicious: {cla}"
-                st.write(message)
-        else:
-            message = f"Your message was flagged as malicious: {cla}"
-            st.write(message)
-
-st.write(text_out)
+        st.write(text_out)
