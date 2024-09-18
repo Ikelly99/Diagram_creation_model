24d05cc1d1310229f17a18111d2b7215d3499b86
Rudolphcljc <rlagunaj@emeal.nttdata.com>
2024-09-18 11:52:28 -0600

    the frontend of the analysis part has been added.

--- architecture_analysis.py
+++ architecture_analysis.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
 st.title("Architecture analysis")
+
+st.set_page_config(
+    page_title="Codd",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="collapsed")
+
+col1, col2 = st.columns([1,1])
+
+
+with col1:
+    uploaded_files = st.file_uploader(
+        "Choose a CSV file", accept_multiple_files=True)
+    txt_input = st.text_area("Enter a short description of the diagram")
+
+
+with col2:
+    st.write(f"You wrote {len(txt)} characters.")
