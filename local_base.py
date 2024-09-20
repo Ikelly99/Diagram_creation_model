94ba424831e28417d87c94175998b726101cabb7
Rudolphcljc <rlagunaj@emeal.nttdata.com>
2024-09-20 17:15:04 -0600

    The function has been modified to overwrite  temporarily saved data.

--- local_base.py
+++ local_base.py
@@ -4,7 +4,6 @@ import polars as pl
 
 def get_values(user_input, lang_response, lang_answer, image):
   route = "C:/Users/rlagunaj/Desktop/FRONT/git/Diagram_creation_model/local_data/first_test.csv" 
-  df_exist = pl.read_csv(route)
   values_dic = {
                 'Timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 
                 'User_input': user_input, 
@@ -13,20 +12,7 @@ def get_values(user_input, lang_response, lang_answer, image):
                 'diagram_image': image
                 }
   df_new = pl.DataFrame(values_dic)
-  df_actualizado = pl.concat([df_exist, df_new])
   
-  df_actualizado.write_csv(route)
+  df_new.write_csv(route)
     
   return "All working"
-
-user_input_p = "I add a some prompt about web architecture"
-
-lang_response = "Here is the explanation of the architecture"
-
-lang_answer = """print("Hello world")"""
-
-image = "some thing"
-
-first_test = get_values(user_input_p, lang_response, lang_answer, image)
-
-print(pl.read_csv("first_test.csv"))
