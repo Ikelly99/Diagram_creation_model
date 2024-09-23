03af651bfb98d226d14f8c3d7e3bebdfb3d4d580
Rudolphcljc <rlagunaj@emeal.nttdata.com>
2024-09-23 13:43:55 -0600

    documentation was added

--- fun_temp_db.py
+++ fun_temp_db.py
@@ -3,16 +3,36 @@ import polars as pl
 import os
 
 def get_values(user_input, lang_response, lang_answer, image):
-  route = os.path.abspath(os.getcwd()) + r"\local_data\first_test.csv"
-  values_dic = {
-                'Timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 
-                'User_input': user_input, 
-                'Response': lang_response, 
-                'diagram_code': lang_answer,
-                'diagram_image': str(image)
-                }
-  df_new = pl.DataFrame(values_dic)
-  
-  df_new.write_csv(route)
+    """
+    This function takes four parameters: user_input, lang_response, lang_answer and image.
+    It creates a dictionary with these values along with a current timestamp.
+    It then converts this dictionary into a DataFrame and saves it to a CSV file.
+
+    Parameters:
+    user_input (str): User input.
+    lang_response (str): Response generated explaining the diagram.
+    lang_answer (str): generated diagram code.
+    image (str): Image of the diagram in string format.
+
+    Returns:
+    str: Confirmation message indicating that everything is working.
+    """
+    # Gets the absolute path to the CSV file where the data will be stored
+    route = os.path.abspath(os.getcwd()) + r"\local_data\first_test.csv"
     
-  return "All working"
+    # Creates a dictionary with the values provided and a timestamp
+    values_dic = {
+        'Timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 
+        'User_input': user_input, 
+        'Response': lang_response, 
+        'diagram_code': lang_answer,
+        'diagram_image': str(image)
+    }
+    
+    # Converts the dictionary to a DataFrame
+    df_new = pl.DataFrame(values_dic)
+    
+    # Writes the DataFrame to a CSV file
+    df_new.write_csv(route)
+    
+    return "All working"
