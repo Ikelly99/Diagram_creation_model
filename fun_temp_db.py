5898d8ae9a0047e35dd63e417e9b4f7c05bd9260
Ikelly99 <ikellyra@emeal.nttdata.com>
2024-09-23 12:42:48 -0600

    path solved

--- fun_temp_db.py
+++ fun_temp_db.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 import polars as pl
-
+import os
 
 def get_values(user_input, lang_response, lang_answer, image):
-  route = "C:/Users/rlagunaj/Desktop/FRONT/git/Diagram_creation_model/local_data/first_test.csv" 
+  route = os.path.abspath(os.getcwd()) + r"\local_data\first_test.csv"
   values_dic = {
                 'Timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 
                 'User_input': user_input, 
                 'Response': lang_response, 
                 'diagram_code': lang_answer,
-                'diagram_image': image
+                'diagram_image': str(image)
                 }
   df_new = pl.DataFrame(values_dic)
   
